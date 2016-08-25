da591eaf600147b18b496ef0c7156368d4600f50
EMAILVISION\Razart.Hasaj <razart.hasaj@smartfocus.com>
2016-08-25 15:31:52 +0100

    Specified the key words in python

--- create_emails.py
+++ create_emails.py
@@ -4,11 +4,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+driver = webdriver.Chrome()
 def create_emails(username, password, http):
-
 	try:
-		driver = webdriver.Chrome()
 		driver.get(http)
 		username = driver.find_element_by_id("IDToken1").send_keys(username)
 		password = driver.find_element_by_id("IDToken2").send_keys(password)
@@ -19,9 +17,18 @@ def create_emails(username, password, http):
 		email = driver.find_element_by_xpath("//li[@class='logo_button engage_button']")
 		email.click()
 		time.sleep(50)
+		return 'engage button'
+	except IOError as e:
+	   print('Error: %s' % e.strerror)
+	   
+def click_create():
+	try:
 		create = driver.find_element_by_id("engage-create")
 		hover = ActionChains(driver).move_to_element(create)
 		hover.perform()
+		time.sleep(10)
+		message_builder = driver.find_element_by_xpath("//ul[@class='dropdown-modules' and @title='Message Builder']/li[@class='dropdown-module']").click()
+		return 'message builder'
 	except IOError as e:
 	   print('Error: %s' % e.strerror)
 
