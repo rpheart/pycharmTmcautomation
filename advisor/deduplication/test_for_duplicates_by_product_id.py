3924be1fc96d2cdbe41c3ebc2b32ea3416d3624b
EMAILVISION\patrick.summers <patrick.summers@smartfocus.com>
2016-10-06 11:45:01 +0100

    add ability for test to read environment variable and run on either qa or preprod based on this. Also added more comments and error checks in functions.

--- advisor/deduplication/test_for_duplicates_by_product_id.py
+++ advisor/deduplication/test_for_duplicates_by_product_id.py
@@ -1,15 +1,27 @@
 import itertools
+import os
 import unittest
 
 import requests
 
 import advisor.utils.api_calls as api
-import advisor.utils.api_qa_settings as settings
+import advisor.utils.api_settings as settings
 
 # Globals
 email = "deduplication@renderer.com"
-engagement = "12843"
 product_list = []
+
+# Build Specific Variables
+if os.environ["BUILD_ENV"] == "QA":
+    renderer = settings.qa_renderer
+    guid = settings.qa_guid
+    engagement = "12843"
+elif os.environ["BUILD_ENV"] == "PREPROD":
+    renderer = settings.preprod_renderer
+    guid = settings.preprod_guid
+    engagement = "6727"
+else:
+    quit()
 
 
 def verify_products_differ(product_id_list):
@@ -18,6 +30,7 @@ def verify_products_differ(product_id_list):
 
     for a, b in itertools.combinations(product_id_list, 2):
         if a is None or b is None or a == b:
+            print "%s and %s are duplicate products" % (a, b)
             return False
 
     return True
@@ -42,7 +55,7 @@ def get_response(renderer_url, guid_link, user_email, engagement_id, position_va
     returns the content of the response"""
 
     request_url = api.offer_open(renderer_url, guid_link, engagement_id, email=user_email, position=position_value,
-                                 height=400, width=400)
+                                 height=400, width=400, format="raw")
     json_handler = requests.get(request_url)  # get the specified url and store the content as a variable
 
     return json_handler.content
@@ -50,65 +63,72 @@ def get_response(renderer_url, guid_link, user_email, engagement_id, position_va
 
 class TestForDuplication(unittest.TestCase):
     def setUp(self):
+        """sends the offer open api 6 times and builds a list of
+        6 product IDs. You can change the length of the list by
+        changing the range value. The duplication checking is dynamic."""
+
         global product_list
 
         for position in range(6):  # range is the number of images you want to generate for comparison
             product_list.append(
-                extract_product_id(get_response(settings.renderer, settings.guid, email, engagement, position + 1)))
+                extract_product_id(get_response(renderer, guid, email, engagement, position + 1)))
 
     def tearDown(self):
+        """clears the product list after each run so that the next test
+        only contains products from a single multi-position offer open"""
+
         global product_list
         product_list = []
 
     def test_if_loop_one_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_two_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_three_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_four_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_five_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_six_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_seven_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_eight_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_nine_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
     def test_if_loop_ten_campaign_snippet_contains_duplicate(self):
         global product_list
 
-        self.assertTrue(verify_products_differ(product_list))
+        self.assertTrue(verify_products_differ(product_list), msg="Duplicates Found")
 
 
 if __name__ == '__main__':
---
+++ advisor/utils/api_settings.py
@@ -0,0 +1,29 @@
+import os
+
+# QA URL Settings
+qa_advisor = "qa-eu1-advisor1-v3.advisor.smartfocus.com"
+qa_renderer = "qa-eu1-p3-renderer.advisor.smartfocus.com"
+qa_click = "qa-eu1-p3-api-public.advisor.smartfocus.com"
+qa_guid = "e44f26b0-12c7-4c29-aabf-9d11197c9c02"
+qa_aid = "20003"
+qa_username = "qaTest1"
+qa_password = "testPass1"
+
+# QA eventbox Kafka settings
+qa_tcp_server = "10.20.187.97"
+qa_tcp_username = "ubuntu"
+qa_tcp_key = os.path.join("C:/keys/qa-eu-master")
+
+# PreProd URL Settings
+preprod_advisor = "preprod-eu1-api-ext-lb-580332958.eu-west-1.elb.amazonaws.com"
+preprod_renderer = "preprod-eu1-p3-renderer.advisor.smartfocus.com"
+preprod_click = "preprod-eu1-p3-api-public.advisor.smartfocus.com"
+preprod_guid = "f04323d7-4224-41b9-8906-17327182c866"
+preprod_aid = "50004"
+preprod_username = "u50004Z1EmS9rs"
+preprod_password = "gHHaHrUYo9SgXuzq"
+
+# PreProd eventbox Kafka settings
+preprod_tcp_server = "10.20.91.101"
+preprod_tcp_username = "psummers"
+preprod_tcp_key = os.path.join("C:/keys/preprod-eu-master")
