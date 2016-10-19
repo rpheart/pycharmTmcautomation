96e36faacba7d3de6edd9d44a69c19bde5335e44
EMAILVISION\patrick.summers <patrick.summers@smartfocus.com>
2016-10-19 15:51:57 +0100

    add client settings as a dictionary and update deduplication test to retrieve json instead of raw

--- advisor/utils/api_settings.py
+++ advisor/utils/api_settings.py
@@ -27,3 +27,70 @@ preprod_password = "gHHaHrUYo9SgXuzq"
 preprod_tcp_server = "10.20.91.101"
 preprod_tcp_username = "psummers"
 preprod_tcp_key = os.path.join("C:/keys/preprod-eu-master")
+
+kafka_settings = {
+    "QA": {
+        "tcp_server": "10.20.187.97",
+        "tcp_username": "ubuntu",
+        "tcp_key": "C:/keys/qa-eu-master"
+    },
+    "PREPROD": {
+        "tcp_server": "10.20.91.101",
+        "tcp_username": "psummers",
+        "tcp_key": "C:/keys/preprod-eu-master"
+    }
+
+}
+
+api_settings = {
+    "QA": {
+        "advisor": "qa-eu1-advisor1-v3.advisor.smartfocus.com",
+        "renderer": "qa-eu1-p3-renderer.advisor.smartfocus.com",
+        "click": "qa-eu1-p3-api-public.advisor.smartfocus.com"
+    },
+    "PREPROD": {
+        "advisor": "preprod-eu1-api-ext-lb-580332958.eu-west-1.elb.amazonaws.com",
+        "renderer": "preprod-eu1-p3-renderer.advisor.smartfocus.com",
+        "click": "preprod-eu1-p3-api-public.advisor.smartfocus.com"
+    }
+}
+
+client_settings = {
+    "QA": {
+        "SIDEV01": {
+            "account name": "SIDEV01",
+            "pod": "E2",
+            "guid": "e44f26b0-12c7-4c29-aabf-9d11197c9c02",
+            "aid": "20003",
+            "username": "qaTest1",
+            "password": "testPass1"
+        },
+        "EMVQA_E1": {
+            "account name": "EMVQA_E1",
+            "pod": "E1",
+            "guid": "35b21e25-6d94-4bd9-a390-6a07c8a6c845",
+            "aid": "20138",
+            "username": "u20135PErkXV8I",
+            "password": "AAA7BxDdA9JYnlyr"
+        }
+    },
+    "PREPROD": {
+        "PREPRODTMC": {
+            "account name": "PREPRODTMC",
+            "pod": "E0",
+            "guid": "f04323d7-4224-41b9-8906-17327182c866",
+            "aid": "50004",
+            "username": "u50004Z1EmS9rs",
+            "password": "gHHaHrUYo9SgXuzq"
+        },
+        "QAPREPROD": {
+            "account name": "QAPREPROD",
+            "pod": "E0",
+            "guid": "c9d07eea-3006-4bc0-aa8f-db490aefd400",
+            "aid": "50010",
+            "username": "u50011KayXn3uW",
+            "password": "VpU1mjvjVN57lxJw"
+        }
+
+    }
+}
