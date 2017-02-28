03c89dfc95dc4614bdc18cd5c39beb1e84298897
patrick.summers <patrick.summers@smartfocus.com>
2017-02-28 13:13:55 +0000

    remove ABCD from test data

--- Emails/UI/Utils/Resources/xss_test_data.py
+++ Emails/UI/Utils/Resources/xss_test_data.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 xss_test_data = [
-    # r"ABCD",
     # r"<>?:\"{}|_+",
     # r"\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f",
     # r"<script>alert(123)</script>",
