ff7fb9a5a0a8fbc26ca00e578d90dcba7b4b6ec1
EMAILVISION\patrick.summers <patrick.summers@smartfocus.com>
2017-03-07 14:08:40 +0000

    add all pods to credentials file

--- Emails/Utils/credentials.py
+++ Emails/Utils/credentials.py
@@ -37,3 +37,143 @@ pod_e1 = {
     "api_password": "qa11focus$",
     "api_key": "CdX7CrxH-EWArkUMb8M_svCiVSoFfM6sjzntS5he2qAcrkZu"
 }
+
+pod_e2 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e2",
+    "ui_password": "qa11focus1.",
+    "api_server": "p2apie.emv3.com",
+    "api_user": "auto_e2_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArkUPb8M_svCiVSoFfM6sjzrtS5tJyJeyLkdL"
+}
+
+pod_e3 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e3",
+    "ui_password": "qa11focus1.",
+    "api_server": "p3apie.emv3.com",
+    "api_user": "auto_e3_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArkUOb8M_svCiVSoFfM6sjzvtS5hhq3zIrkbi"
+}
+
+pod_e4 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e4",
+    "ui_password": "qa11focus1.",
+    "api_server": "p4apie.emv3.com",
+    "api_user": "auto_e4_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArkUJb8M_svCiVSoFfM6sjzztS5hfOQbwrkb-"
+}
+
+pod_e5 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e5",
+    "ui_password": "qa11focus1.",
+    "api_server": "p5apie.emv3.com",
+    "api_user": "auto_e5_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrFH-EWArkUIb8M_svCiVSoFfM6smkepDqwGaPLJxOBPR5qD"
+}
+
+pod_e6 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e6",
+    "ui_password": "qa11focus1.",
+    "api_server": "p6apie.emv3.com",
+    "api_user": "auto_e6_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArkULb8M_svCiVSoFfM6sjz7tS5h06anyLkcM"
+}
+
+pod_e7 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e7",
+    "ui_password": "qa11focus1.",
+    "api_server": "p7apie.emv3.com",
+    "api_user": "auto_e7_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArkUKb8M_svCiVSoFfM6sjz_tS5hhq3zIrkYa"
+}
+
+pod_e8 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e8",
+    "ui_password": "qa11focus1.",
+    "api_server": "p8apie.emv3.com",
+    "api_user": "auto_e8_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArkUFb8M_svCiVSoFfM6sjzDtS5jBk83srkYt"
+}
+
+pod_e9 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_e9",
+    "ui_password": "qa11focus1.",
+    "api_server": "p9apie.emv3.com",
+    "api_user": "auto_e9_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArkUEb8M_svCiVSoFfM6sjzHtS5iSdgJkrkZ8"
+}
+
+pod_t1 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_t1",
+    "ui_password": "qa11focus1.",
+    "api_server": "emvapi.emv2.com",
+    "api_user": "auto_t1_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArlQMb8M_svCiVSoFfM6snjntS5hfOQbwrkbm"
+}
+
+pod_t2 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_t2",
+    "ui_password": "qa11focus1.",
+    "api_server": "p2apic.emv2.com",
+    "api_user": "auto_t2_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArlQPb8M_svCiVSoFfM6snjrtS5hfOQbwrkbk"
+}
+
+pod_t3 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_t3",
+    "ui_password": "qa11focus1.",
+    "api_server": "p3apic.emv2.com",
+    "api_user": "auto_t3_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArlQOb8M_svCiVSoFfM6snjvtS5iSdgJkrkZq"
+}
+
+pod_t4 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_t4",
+    "ui_password": "qa11focus1.",
+    "api_server": "p4apic.emv2.com",
+    "api_user": "auto_t4_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArlQJb8M_svCiVSoFfM6snjztS5hcAP1orkaL"
+}
+
+pod_t5 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "auto_t5",
+    "ui_password": "qa11focus1.",
+    "api_server": "p5apic.emv2.com",
+    "api_user": "auto_t5_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArlQIb8M_svCiVSoFfM6snj3tS5h2-91grkYe"
+}
+
+pod_t6 = {
+    "ui_server": "https://sfui.themessagecloud.com",
+    "ui_username": "",
+    "ui_password": "",
+    "api_server": "p6apic.emv3.com",
+    "api_user": "",
+    "api_password": "",
+    "api_key": ""
+}
