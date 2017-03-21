5627f84524ebc11121bbe643f30ad34312d74fdd
EMAILVISION\patrick.summers <patrick.summers@smartfocus.com>
2017-03-21 16:30:35 +0000

    update credentials file for sending apis and fix vtrunk expired credentials

--- Emails/Utils/credentials.py
+++ Emails/Utils/credentials.py
@@ -1,11 +1,13 @@
 vtrunk = {
     "ui_server": "https://qa-sfui.themessagecloud.com",
     "ui_username": "sidev_qa",
-    "ui_password": "qa11focus1.",
+    "ui_password": "computer1.",
     "api_server": "p2itgapie.emv2.com",
     "api_user": "sidev_qa_api",
-    "api_password": "qa11focus$",
-    "api_key": "CdX7Cr1V5FWKh39MUf0uq5PtSzUOacvD-wjtSiVihvVpJzE"
+    "api_password": "computer1.",
+    "api_key": "CdX7Cr1V5FWKh39MUf0uq5PtSzUOacvD-wjtSiVihvVpJzE",
+    "marketing_verified_email": "email@sidev01.cccampaigns.net",
+    "trans_verified_email": "email@sidev01.nmp1.com",
 }
 
 vnext = {
@@ -15,7 +17,9 @@ vnext = {
     "api_server": "p1itgapie.emv2.com",
     "api_user": "emvqa_qa_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxD4EeekH9MUf0uq5PtXTEcfdysjzntS4SNd1hmLkbF"
+    "api_key": "CdX7CrxD4EeekH9MUf0uq5PtXTEcfdysjzntS4SNd1hmLkbF",
+    "marketing_verified_email": "test@emvqae1.ccemails.net",
+    "trans_verified_email": "email@emvqae1.nmp1.com",
 }
 
 preprod = {
@@ -25,7 +29,9 @@ preprod = {
     "api_server": "p0apie.emv3.com",
     "api_user": "preprod_qa_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrBW_1Sfg09Zb9MuhJuXcXYaftijmEepH9RFaPDoulZS9hGSRg"
+    "api_key": "CdX7CrBW_1Sfg09Zb9MuhJuXcXYaftijmEepH9RFaPDoulZS9hGSRg",
+    "marketing_verified_email": "email@preprodtmc.ccemails.net",
+    "trans_verified_email": "email@preprodtmc.nmp1.com",
 }
 
 pod_e1 = {
@@ -35,7 +41,9 @@ pod_e1 = {
     "api_server": "emvapi.emv3.com",
     "api_user": "auto_e1_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkUMb8M_svCiVSoFfM6sjzntS5he2qAcrkZu"
+    "api_key": "CdX7CrxH-EWArkUMb8M_svCiVSoFfM6sjzntS5he2qAcrkZu",
+    "marketing_verified_email": "email@emvops-e1.cccampaigns.net",
+    "trans_verified_email": "email@emvops-e1.nmp1.com",
 }
 
 pod_e2 = {
@@ -45,7 +53,9 @@ pod_e2 = {
     "api_server": "p2apie.emv3.com",
     "api_user": "auto_e2_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkUPb8M_svCiVSoFfM6sjzrtS5tJyJeyLkdL"
+    "api_key": "CdX7CrxH-EWArkUPb8M_svCiVSoFfM6sjzrtS5tJyJeyLkdL",
+    "marketing_verified_email": "email@emvops-e2.cccampaigns.com",
+    "trans_verified_email": "email@emvops-e2.nmp1.net",
 }
 
 pod_e3 = {
@@ -55,7 +65,9 @@ pod_e3 = {
     "api_server": "p3apie.emv3.com",
     "api_user": "auto_e3_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkUOb8M_svCiVSoFfM6sjzvtS5hhq3zIrkbi"
+    "api_key": "CdX7CrxH-EWArkUOb8M_svCiVSoFfM6sjzvtS5hhq3zIrkbi",
+    "marketing_verified_email": "email@emvops-e3.cccampaigns.net",
+    "trans_verified_email": "email@emvops-e3.nmp1.com",
 }
 
 pod_e4 = {
@@ -65,7 +77,9 @@ pod_e4 = {
     "api_server": "p4apie.emv3.com",
     "api_user": "auto_e4_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkUJb8M_svCiVSoFfM6sjzztS5hfOQbwrkb-"
+    "api_key": "CdX7CrxH-EWArkUJb8M_svCiVSoFfM6sjzztS5hfOQbwrkb-",
+    "marketing_verified_email": "email@emvops-e4.cccampaigns.net",
+    "trans_verified_email": "email@emvops-e4.nmp1.com",
 }
 
 pod_e5 = {
@@ -75,7 +89,9 @@ pod_e5 = {
     "api_server": "p5apie.emv3.com",
     "api_user": "auto_e5_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrFH-EWArkUIb8M_svCiVSoFfM6smkepDqwGaPLJxOBPR5qD"
+    "api_key": "CdX7CrFH-EWArkUIb8M_svCiVSoFfM6smkepDqwGaPLJxOBPR5qD",
+    "marketing_verified_email": "email@techopsteam.emv9.net",
+    "trans_verified_email": "email@emvops_pode5.nmp1.com",
 }
 
 pod_e6 = {
@@ -85,7 +101,9 @@ pod_e6 = {
     "api_server": "p6apie.emv3.com",
     "api_user": "auto_e6_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkULb8M_svCiVSoFfM6sjz7tS5h06anyLkcM"
+    "api_key": "CdX7CrxH-EWArkULb8M_svCiVSoFfM6sjz7tS5h06anyLkcM",
+    "marketing_verified_email": "email@emvops-e6.cccampaigns.net",
+    "trans_verified_email": "email@emvops-e6.nmp1.com",
 }
 
 pod_e7 = {
@@ -95,7 +113,9 @@ pod_e7 = {
     "api_server": "p7apie.emv3.com",
     "api_user": "auto_e7_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkUKb8M_svCiVSoFfM6sjz_tS5hhq3zIrkYa"
+    "api_key": "CdX7CrxH-EWArkUKb8M_svCiVSoFfM6sjz_tS5hhq3zIrkYa",
+    "marketing_verified_email": "email@emvops-e7.cccampaigns.net",
+    "trans_verified_email": "email@emvops-e7.nmp1.com",
 }
 
 pod_e8 = {
@@ -105,7 +125,9 @@ pod_e8 = {
     "api_server": "p8apie.emv3.com",
     "api_user": "auto_e8_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkUFb8M_svCiVSoFfM6sjzDtS5jBk83srkYt"
+    "api_key": "CdX7CrxH-EWArkUFb8M_svCiVSoFfM6sjzDtS5jBk83srkYt",
+    "marketing_verified_email": "email@emvops-e8.cccampaigns.net",
+    "trans_verified_email": "email@emvops-e8.nmp1.com",
 }
 
 pod_e9 = {
@@ -115,7 +137,9 @@ pod_e9 = {
     "api_server": "p9apie.emv3.com",
     "api_user": "auto_e9_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArkUEb8M_svCiVSoFfM6sjzHtS5iSdgJkrkZ8"
+    "api_key": "CdX7CrxH-EWArkUEb8M_svCiVSoFfM6sjzHtS5iSdgJkrkZ8",
+    "marketing_verified_email": "email@emvops-e9.ccemails.net",
+    "trans_verified_email": "email@emvops-e9.nmp1.com",
 }
 
 pod_t1 = {
@@ -125,7 +149,9 @@ pod_t1 = {
     "api_server": "emvapi.emv2.com",
     "api_user": "auto_t1_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArlQMb8M_svCiVSoFfM6snjntS5hfOQbwrkbm"
+    "api_key": "CdX7CrxH-EWArlQMb8M_svCiVSoFfM6snjntS5hfOQbwrkbm",
+    "marketing_verified_email": "email@emvops-t1.cccampaigns.com",
+    "trans_verified_email": "email@emvops-t1.nmp1.com",
 }
 
 pod_t2 = {
@@ -135,7 +161,9 @@ pod_t2 = {
     "api_server": "p2apic.emv2.com",
     "api_user": "auto_t2_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArlQPb8M_svCiVSoFfM6snjrtS5hfOQbwrkbk"
+    "api_key": "CdX7CrxH-EWArlQPb8M_svCiVSoFfM6snjrtS5hfOQbwrkbk",
+    "marketing_verified_email": "email@emvops-t2.cccampaigns.com",
+    "trans_verified_email": "email@emvops-t2.nmp1.com",
 }
 
 pod_t3 = {
@@ -145,7 +173,9 @@ pod_t3 = {
     "api_server": "p3apic.emv2.com",
     "api_user": "auto_t3_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArlQOb8M_svCiVSoFfM6snjvtS5iSdgJkrkZq"
+    "api_key": "CdX7CrxH-EWArlQOb8M_svCiVSoFfM6snjvtS5iSdgJkrkZq",
+    "marketing_verified_email": "email@emvops-t3.cccampaigns.com",
+    "trans_verified_email": "email@emvops-t3.nmp1.com",
 }
 
 pod_t4 = {
@@ -155,7 +185,9 @@ pod_t4 = {
     "api_server": "p4apic.emv2.com",
     "api_user": "auto_t4_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArlQJb8M_svCiVSoFfM6snjztS5hcAP1orkaL"
+    "api_key": "CdX7CrxH-EWArlQJb8M_svCiVSoFfM6snjztS5hcAP1orkaL",
+    "marketing_verified_email": "email@emvops-t4.cccampaigns.com",
+    "trans_verified_email": "email@emvops-t4.nmp1.com",
 }
 
 pod_t5 = {
@@ -165,15 +197,19 @@ pod_t5 = {
     "api_server": "p5apic.emv2.com",
     "api_user": "auto_t5_api",
     "api_password": "qa11focus$",
-    "api_key": "CdX7CrxH-EWArlQIb8M_svCiVSoFfM6snj3tS5h2-91grkYe"
+    "api_key": "CdX7CrxH-EWArlQIb8M_svCiVSoFfM6snj3tS5h2-91grkYe",
+    "marketing_verified_email": "email@emvops-t5.cccampaigns.com",
+    "trans_verified_email": "email@emvops-t5.nmp1.com",
 }
 
 pod_t6 = {
     "ui_server": "https://sfui.themessagecloud.com",
-    "ui_username": "",
-    "ui_password": "",
+    "ui_username": "auto_t6",
+    "ui_password": "qa11focus1.",
     "api_server": "p6apic.emv3.com",
-    "api_user": "",
-    "api_password": "",
-    "api_key": ""
+    "api_user": "auto_t6_api",
+    "api_password": "qa11focus$",
+    "api_key": "CdX7CrxH-EWArlQLb8M_svCiVSoFfM6snj7tS5hrCNOeLkcu",
+    "marketing_verified_email": "email@emvops-t6.ccemails.com",
+    "trans_verified_email": "email@emvops-t6.nmp1.com",
 }
